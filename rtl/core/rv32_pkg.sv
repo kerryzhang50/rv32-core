de1e09c6f16df82d367fefe90a3667498bd19e88
kerryzhang50 <zkerry50@gmail.com>
2026-07-28 16:46:03 -0400

    Expanded rv32_pkg to include RISC-V Opcodes and funct3 and funct7 enums

--- rtl/core/rv32_pkg.sv
+++ rtl/core/rv32_pkg.sv
@@ -15,4 +15,37 @@ package rv32_pkg;
 
     } alu_op_t;
 
+    typedef enum logic [6:0] {
+
+        OPCODE_LOAD    = 7'b0000011,
+        OPCODE_STORE   = 7'b0100011,
+        OPCODE_BRANCH  = 7'b1100011,
+        OPCODE_JALR    = 7'b1100111,
+        OPCODE_JAL     = 7'b1101111,
+        OPCODE_OP_IMM  = 7'b0010011,
+        OPCODE_OP      = 7'b0110011,
+        OPCODE_LUI     = 7'b0110111,
+        OPCODE_AUIPC   = 7'b0010111
+
+    } opcode_t;
+
+    typedef enum logic [2:0] {
+
+        F3_ADD_SUB = 3'b000,
+        F3_SLL     = 3'b001,
+        F3_SLT     = 3'b010,
+        F3_SLTU    = 3'b011,
+        F3_XOR     = 3'b100,
+        F3_SRL_SRA = 3'b101,
+        F3_OR      = 3'b110,
+        F3_AND     = 3'b111
+
+    } funct3_alu_t;
+
+    typedef enum logic [6:0] {
+
+        F7_NORMAL = 7'b0000000,
+        F7_ALT    = 7'b0100000
+
+    } funct7_t;
 endpackage
