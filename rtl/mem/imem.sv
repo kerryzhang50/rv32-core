5537234f386e742ba5bfe2235e34b019eb86113f
kerryzhang50 <zkerry50@gmail.com>
2026-08-03 10:58:56 -0400

    Changed imem to read memory from hex file

--- rtl/mem/imem.sv
+++ rtl/mem/imem.sv
@@ -1,9 +1,18 @@
 module imem
+#(
+    parameter string INIT_FILE = "program.hex"
+)
 (
     input  logic [31:0] addr,
     output logic [31:0] instruction
 );
-    initial begin
-        $readmemh("program.hex", memory);
-    end
+
+logic [31:0] memory [0:255];
+
+initial begin
+    $readmemh(INIT_FILE, memory);
+end
+
+assign instruction = memory[addr[9:2]];
+
 endmodule
