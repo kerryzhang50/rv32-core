9f248837ea5966b3c0bd83a254563c4f2fa647b2
kerryzhang50 <zkerry50@gmail.com>
2026-08-06 16:44:50 -0400

    Added more exposed debug variables

--- sim/sim_top.sv
+++ sim/sim_top.sv
@@ -7,24 +7,22 @@ module sim_top
     output logic [31:0] dbg_instr,
     output logic        dbg_reg_write,
     output logic [4:0]  dbg_rd,
-    output logic [31:0] dbg_write_data
+    output logic [31:0] dbg_write_data,
+
+    output  logic        req_valid,
+    output  logic        req_write,
+    output  logic [31:0] req_addr,
+    output  logic [31:0] req_wdata,
+    output  logic [3:0]  req_wstrb,
+
+    output logic        resp_valid,
+    output logic [31:0] resp_rdata,
+
+    output logic        dbg_stall
 );
 
 logic [31:0] imem_addr;
 logic [31:0] imem_rdata;
-
-logic [31:0] odbg_pc;
-logic [31:0] odbg_instr;
-
-logic        odbg_reg_write;
-logic [4:0]  odbg_rd;
-logic [31:0] odbg_write_data;
-
-assign dbg_pc = odbg_pc;
-assign dbg_instr = odbg_instr;
-assign dbg_reg_write = odbg_reg_write;
-assign dbg_rd = odbg_rd;
-assign dbg_write_data = odbg_write_data;
 
 rv32_core core(
     .clk(clk),
@@ -33,15 +31,27 @@ rv32_core core(
     .imem_addr(imem_addr),
     .imem_rdata(imem_rdata),
 
-    .dbg_pc(odbg_pc),
-    .dbg_instr(odbg_instr),
-    .dbg_reg_write(odbg_reg_write),
-    .dbg_rd(odbg_rd),
-    .dbg_write_data(odbg_write_data)
+    .dbg_pc(dbg_pc),
+    .dbg_instr(dbg_instr),
+    .dbg_reg_write(dbg_reg_write),
+    .dbg_rd(dbg_rd),
+    .dbg_write_data(dbg_write_data),
+
+    .req_valid(req_valid),
+    .req_write(req_write),
+    .req_addr(req_addr),
+    .req_wdata(req_wdata),
+    .req_wstrb(req_wstrb),
+
+    .resp_valid(resp_valid),
+    .resp_rdata(oresp_rdata),
+
+    .dbg_stall(dbg_stall)
+
 );
 
 imem #(
-    .INIT_FILE("../tests/hex/add_test.hex")
+    .INIT_FILE("../tests/hex/ls_test.hex")
 ) imem0(
     .addr(imem_addr),
     .instruction(imem_rdata)
